3b71d05ac7a6c3f4e79b76a917fb787408c022b5
max <nyxdesu3@gmail.com>
2026-03-04 12:58:45 +0800

    images from internet

--- ReactProjects/testing/src/components/gallery.tsx
+++ ReactProjects/testing/src/components/gallery.tsx
@@ -1,28 +1,30 @@
+import { useState } from "react";
+
 const images = [
-    "src/assets/img/pic_2.jpg",
-    "src/assets/img/pic_3.jpg",
-    "src/assets/img/pic_4.jpg",
-    "src/assets/img/pic_5.jpg",
-    "src/assets/img/pic_6.jpg",
-    "src/assets/img/pic_7.jpg",
-    "src/assets/img/pic_8.jpg",
-    "src/assets/img/pic_9.jpg",
-    "src/assets/img/pic_10.jpg",
-    "src/assets/img/pic_11.jpg",
-    "src/assets/img/pic_12.jpg",
-    "src/assets/img/pic_13.jpg",
-    "src/assets/img/pic_14.jpg",
-    "src/assets/img/pic_15.jpg",
-    "src/assets/img/pic_16.jpg",
-    "src/assets/img/pic_17.jpg",
-    "src/assets/img/pic_18.jpg",
-    "src/assets/img/pic_19.jpg",
-    "src/assets/img/pic_20.jpg",
-    "src/assets/img/pic_21.jpg",
-    "src/assets/img/pic_22.jpg",
-    "src/assets/img/pic_23.jpg",
-    "src/assets/img/pic_24.jpg",
-    "src/assets/img/pic_25.jpg"]
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_2.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_3.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_4.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_5.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_6.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_7.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_8.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_9.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_10.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_11.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_12.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_13.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_14.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_15.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_16.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_17.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_18.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_19.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_20.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_21.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_22.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_23.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_24.jpg",
+    "https://davidortizjr.github.io/old-portfolio/school/webdev/Gallery/memory_lane/pic_25.jpg"]
 
 const tapeColors = [
     "var(--clr-pink)",
@@ -33,18 +35,27 @@ const tapeColors = [
 ];
 
 const tapePositions = [
-    "left-4, rotate-3",
-    "right-4 -rotate-3",
-    "left-4 -translate-y-1/2 rotate-6",
-    "right-4 -translate-y-1/2 -rotate-6",
-    "left-4 -translate-y-full rotate-12",
-    "right-4 -translate-y-full -rotate-12"
+    "left-4 rotate-3 top-0",
+    "right-4 -rotate-3 top-0",
+    "left-4 -translate-y-1/2 rotate-6 top-0",
+    "right-4 -translate-y-1/2 -rotate-6 top-0",
+    "left-4 -translate-y-full rotate-12 top-0",
+    "right-4 -translate-y-full -rotate-12 top-0"
 ];
 
+function shuffleArray(array: string[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
 export function Gallery() {
+    const [shuffledImages] = useState(() => shuffleArray(images));
     return (
         <div className="columns-1 md:columns-2 lg:columns-3 gap-10 space-y-12 py-8">
-            {images.slice(1).map((src, index) => (
+            {shuffledImages.map((src, index) => (
                 <div key={index} className="relative group inline-block w-80">
                     <div className="frame shadow-lg border border-slate-100 transition-transform transform group-hover:scale-105">
                         <div className={`tape ${tapePositions[index % tapePositions.length]}`} style={{ backgroundColor: tapeColors[index % tapeColors.length] }}></div>
@@ -52,7 +63,6 @@ export function Gallery() {
                     </div>
                 </div>
             ))}
-
         </div>
     )
 }
